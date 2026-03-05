bcb16e703204d3e8d9449a87cfbf9c8d3082f405
virtualveteran <estheroswald001@yahoo.com>
2026-03-05 10:09:09 -0700

    creates prepare questions

--- quiz_engine.py
+++ quiz_engine.py
@@ -2,23 +2,24 @@ import random
 
 filepath = question_generator.py
 
-Question_Path = filepath
+question_path = filepath
 
 Number_Questions_Per_Quiz = 5
 
-def Run_Quiz():
-    questions = Prepare_Questions(Question_Path)
+def run_quiz():
+    questions = prepare_questions(question_path)
 
     number_correct = 0
     for number, question in enumerate(questions, start=1):
         print(f"\nQuestion {num}:")
-        number_correct += Ask_Question(question)
+        number_correct += ask_question(question)
     print(f"\nYou got {number_correct} correct out of {number} questions!")
 
 
 
-def Prepare_Questions(filepath, number_questions):
+def prepare_questions(filepath, number_questions):
     with open(filepath, "r") as file:
-        for questions in file:
-            number_questions = min(number_questions, len(questions))
-            return random.sample(questions, k=number_questions)
+        questions = file.readlines()
+
+    number_questions = min(number_questions, len(questions))
+    return random.sample(questions, k=number_questions)
