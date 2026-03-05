08bbdfbd3c0410bc187464081dcf4178c89cd0a7
virtualveteran <estheroswald001@yahoo.com>
2026-03-05 10:43:23 -0700

    adds in error handling for wrong answers

--- quiz_engine.py
+++ quiz_engine.py
@@ -6,6 +6,10 @@ question_path = filepath
 
 Number_Questions_Per_Quiz = 5
 
+correct = answer
+
+wrong = alternative 
+
 def run_quiz():
     questions = prepare_questions(question_path)
 
@@ -27,17 +31,63 @@ def prepare_questions(filepath, number_questions):
     return random.sample(questions, k=number_questions)
 
 
+
+# Here I am trying to make the correct answer list and the wrong answer list, and take a random set of wrong answers
+# as well as get the list of right answers for each question, and create an error message when they are answered incorrectly
+
+
 def ask_question(question):
-    correct_answer = question["correct"]
-    wrong = question["correct"] + question["wrong"]
+    correct_answer = question["answer"]
+    alternative = question["answer"] + question["alternative"]
     ordered_wrong = random.sample(wrong, k=len(wrong))
 
     correct = get_answers(
         question=question["question"]
-        wrong=ordered_wrong,
+        alternative=ordered_alternative,
         number_choices=len(correct_answer)
     
     )
 
-    if correct := (set(correct)) == set(correct_answer):
-        print("🏆  CORRECT ANSWER!")
+    if correct := (set(answers)) == set(correct_answer):
+        print("🏆  CORRECT ANSWER! 🏆 ")
+    else:
+        if len(correct_answer) == 1:
+            print("No, the answer is:")
+        else:
+            print("No, the answeres are:")
+
+        for answer in correct_answer:
+            print("-", correct)
+
+
+
+def get_answer(question, alternatives, number_choices=1):
+    print(f"{question}?")
+    labeled_alternatives = dict(alternatives, strict=False)
+
+    for alternatives in labeled_alternatives.items():
+        print(f" {lable} {alternatives}")
+
+    while True:
+        plural_s = "" if number_choices == 1 else f"s" (choose {number_choices})
+        answer = input(f"\nChoice{plural_s}?")
+        answer = set(answer.replace(",", "").split())
+
+
+        if len(answer) != number_choices:
+         plural_s = "" if number_choices == 1 else "s, separated by comma"
+         print(f"Please answer {number_choices} alternative{plural_s}")
+         continue
+
+
+        if any(
+            (invalid := answer) not in labeled_alternatives
+            for answer in answers):
+             
+             print (
+                 f"{invalid!r} is not the right choice."
+                 f"Please use {','.join(labeled_alternatives)}"
+             )
+             continue
+        return [labeled_alternatives[answer] for answer in answers]
+    
